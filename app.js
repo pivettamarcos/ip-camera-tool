9ba6b92bda5fda2cf47293c4eb4336dd416da5d6
Marcos Pivetta <marcospivetta60@gmail.com>
2018-05-12 01:03:39 -0300

    latest

--- app.js
+++ app.js
@@ -1,18 +1,47 @@
-Stream = require('node-rtsp-stream');
-stream = new Stream({
-    name: 'name',
-    streamUrl: 'rtsp://184.72.239.149/vod/mp4:BigBuckBunny_115k.mov',
-    wsPort: 9999
+const onvif = require('node-onvif');
+const interval = require('interval-promise')
+const fs = require('fs')
+var VisualRecognitionV3 = require('watson-developer-cloud/visual-recognition/v3');
+
+
+let watsonCredentials = {
+    "url": "https://gateway-a.watsonplatform.net/visual-recognition/api",
+    "api_key": "4521f693efb6296db095cbb5365ef1299f533c79"
+  };
+
+const discovery = new VisualRecognitionV3({
+    url: watsonCredentials.url,
+    version: '2018-05-11',
+    api_key: watsonCredentials.api_key,
 });
 
-Websocket = require('ws');
-jsmpeg = require('jsmpeg');
-
-
-
-client = new Websocket('ws://localhost:9999');
-player = new jsmpeg(client, {
-    canvas: canvas // Canvas should be a canvas DOM element
+let device = new onvif.OnvifDevice({
+    xaddr: 'pivetta.ddns.net:8080',
+    user: 'admin',
+    pass: 'admin'
 });
 
-console.log(stream);
+// Initialize the OnvifDevice object
+device.init().then(() => {
+    // Get the data of the snapshot
+    console.log('fetching the data of the snapshot...');
+
+    discovery.setAccessToken('<access-token>')
+
+
+    interval(async () => {
+        await device.fetchSnapshot().then( (res) => {
+            fs.writeFileSync("testPhotos/"+res.headers.date.replace(/:/g,"=")+".jpeg", res.body, {encoding: 'binary'});
+        }).catch((error) => {
+            console.error(error);
+        });
+    }, 10, {iterations: 15})
+    
+    //return device.fetchSnapshot();
+  })/*.then((res) => {
+    // Save the data to a file
+    fs.writeFileSync('snapshot.jpg', res.body, {encoding: 'binary'});
+    console.log('Done!');
+  }).catch((error) => {
+    console.error(error);
+  });*/
